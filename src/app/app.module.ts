8ca87c5925bef3b9b8c76e25b47e6501f0404709
NoelMillan <noel.millan.r@hotmail.com>
2021-12-09 14:40:29 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ButtonModule } from 'primeng/button';
+import { PanelModule } from 'primeng/panel';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {CardModule} from 'primeng/card';
+import {TableModule} from 'primeng/table';
+import {BadgeModule} from 'primeng/badge';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
+import { CentersComponent } from './components/centers/centers.component';
+import { IndicatorsComponent } from './components/indicators/indicators.component';
+import { AboutUsComponent } from './components/about-us/about-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    CentersComponent,
+    IndicatorsComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ButtonModule,
+    PanelModule,
+    CardModule,
+    TableModule,
+    BadgeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { UsersService } from 'src/app/services/users.service';
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  users: User[] = [];
+
+  constructor(public userService: UsersService) {
+    this.users = this.userService.users;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
